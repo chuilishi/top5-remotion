38cd529e5c5c0bc2dbb8662f12dcb8152ca59615
chuilishi <1483843127@qq.com>
2026-03-26 00:02:25 +0800

    chore: update config, gitignore, and misc project files

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,8 +1,7 @@
 import { Config } from "@remotion/cli/config";
 import os from "os";
 
-Config.setVideoImageFormat("jpeg");
-Config.setJpegQuality(75);
+Config.setVideoImageFormat("png");
 Config.setOverwriteOutput(true);
 Config.setConcurrency(16);
 Config.setChromiumOpenGlRenderer("angle");
