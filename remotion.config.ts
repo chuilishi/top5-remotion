998d726de1d618f4e44f03215028634f6e1b50e2
chuilishi <1483843127@qq.com>
2026-03-28 19:08:29 +0800

    perf: 截图格式 PNG→JPEG quality=100（渲染快 43%）

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,7 +1,8 @@
 import { Config } from "@remotion/cli/config";
 import os from "os";
 
-Config.setVideoImageFormat("png");
+Config.setVideoImageFormat("jpeg");
+Config.setJpegQuality(100);
 Config.setOverwriteOutput(true);
 Config.setConcurrency(16);
 Config.setChromiumOpenGlRenderer("angle");
